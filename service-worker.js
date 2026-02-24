6398849f33de9ca9ecebd0882dbd7f3adfec58d9
luanhnn25-jpg <luanhnn25@gmail.com>
2026-02-24 00:15:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,36 @@
+const CACHE_NAME = "meusite-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/offline.html"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return res;
+      })
+      .catch(() =>
+        caches.match(event.request).then((cached) => cached || caches.match("/offline.html"))
+      )
+  );
+});
