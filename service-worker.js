cba4258ff5f302ca1e0c2f279dc818e2ae7baff8
luanhnn25-jpg <luanhnn25@gmail.com>
2026-02-25 03:44:18 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lpp-v9"; // troque a versão quando atualizar
+const CACHE_NAME = "lpp-v1"; // troque a versão quando atualizar
 
 // Cache mínimo garantido (caminhos relativos para GitHub Pages /lesa/)
 const CORE_ASSETS = [
